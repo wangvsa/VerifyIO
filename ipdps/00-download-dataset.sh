d7b238ede3f7eda74cf65aa090f9831a7221e524
Chen Wang <wangvsa@gmail.com>
2024-12-27 09:48:26 -0800

    Remove -v flag
    Signed-off-by: Chen Wang <wangvsa@gmail.com>

--- ipdps/00-download-dataset.sh
+++ ipdps/00-download-dataset.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 DATASET_DIR=./dataset
 mkdir ${DATASET_DIR}
@@ -6,8 +6,8 @@ cd ${DATASET_DIR}
 wget https://zenodo.org/records/14553174/files/hdf5-1.14.4-3-recorder-traces.tar.gz 
 wget https://zenodo.org/records/14553174/files/netcdf-4.9.2-recorder-traces.tar.gz
 wget https://zenodo.org/records/14553174/files/pnetcdf-1.13.0-recorder-traces.tar.gz
-tar -zxvf hdf5-1.14.4-3-recorder-traces.tar.gz
-tar -zxvf netcdf-4.9.2-recorder-traces.tar.gz
-tar -zxvf pnetcdf-1.13.0-recorder-traces.tar.gz
+tar -zxf hdf5-1.14.4-3-recorder-traces.tar.gz
+tar -zxf netcdf-4.9.2-recorder-traces.tar.gz
+tar -zxf pnetcdf-1.13.0-recorder-traces.tar.gz
 rm -rf *.tar.gz
 cd ../
