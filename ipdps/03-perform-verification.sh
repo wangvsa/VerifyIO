6d3df5216abac517ed8bbf1e8ec4799af059d801
Chen Wang <wangvsa@gmail.com>
2025-01-10 11:29:11 -0800

    Directly write result to csv files
    Signed-off-by: Chen Wang <wangvsa@gmail.com>

--- ipdps/03-perform-verification.sh
+++ ipdps/03-perform-verification.sh
@@ -6,7 +6,7 @@
 #SBATCH -o /path/to/out.out
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <base_directory>"
+    echo "Usage: $0 <library_trace_directory>"
     exit 1
 fi
 
@@ -17,16 +17,28 @@ if [[ ! -f "$PROGRAM" ]]; then
     exit 1
 fi
 
-
 SEMANTICS=("POSIX" "MPI-IO" "Commit" "Session")
-#SEMANTICS=("POSIX" "MPI-IO")
+
+# Write all verifyio.py output to a single temporary text file first
+LIB_TRACE_DIR=$(basename ${BASE_DIR})
+ARR=(${LIB_TRACE_DIR//-/ })
+LIB_NAME=${ARR[1]}
+TEXT_RESULT_FILE=/tmp/${LIB_NAME}.txt
+rm -f ${TEXT_RESULT_FILE}
+
 
 for dir in "$BASE_DIR"/*/; do
     if [ -d "$dir" ]; then
         echo "Perform verification on $dir"
         for semantic in "${SEMANTICS[@]}"; do
-            python3 "$PROGRAM" "$dir" "--semantics=$semantic"
+            python3 $PROGRAM $dir --semantics=$semantic | tee -a $TEXT_RESULT_FILE
         done
-	echo "==============================================="
+        echo "==============================================="
     fi
 done
+
+# Conver the text output of all tests to a single CSV file
+mkdir -p ./result
+CSV_RESULT_FILE=./result/${LIB_NAME}.csv
+python3 $VERIFYIO_INSTALL_PATH/verifyio_to_csv.py $file $output --group_by_api
+
